59202af27fb090eee9da7f637c52c5740df55454
Slavyanine <55347035+Slavyanine@users.noreply.github.com>
2025-02-21 11:20:08 +0300

    refactor the add_vector method

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -5,3 +5,20 @@ Console.WriteLine("Test 4");
 Console.WriteLine("Test 5");
 Console.WriteLine("Test 6");
 Console.WriteLine("Test 7");
+
+/// <summary>
+/// Calculate the body mass index (BMI) based on weight in kg and height in meter
+/// </summary>
+/// <param name="weight">
+/// The weight in kilogram
+/// </param>
+/// <param name="height">
+/// The height in meter
+/// </param>
+/// <returns>
+/// The body mass index
+/// </returns>
+double CalculateBMI(double weight, double height)
+{
+    return weight / (height * height);
+}
